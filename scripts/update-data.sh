2638b7c91bfea3845fbd6af262d7a7bb61295b18
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-01-07 05:27:35 +0530

    Parse data after it has been downloaded

--- scripts/update-data.sh
+++ scripts/update-data.sh
@@ -5,10 +5,12 @@ set -euxo pipefail
 PUSH=${1:-}
 date
 python scripts/download-data.py
+python scripts/parse-data.py
 if [ "${PUSH}" = "push" ]; then
     git config user.email "noreply@indiaultimate.org"
     git config user.name "India Ultimate GitHub Bot"
     git add data/
+    git add public/data/
     git commit -m "Auto update data for the website" || echo "Nothing to commit"
     git push origin main
 fi
