d86aa2810a87300a5487c9a82f913fe362b9dfb2
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-01-07 02:44:38 +0530

    Add script to download data for current NCS tournament

---
+++ scripts/download-data.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+import requests
+
+ROOT_DIR = Path(__file__).parent.parent.absolute()
+RAW_DATA_DIR = ROOT_DIR.joinpath("data", "raw")
+
+
+def get_player_stats(sheet_id, sheet_name, tournament_name):
+    print(f"Downloading '{sheet_name}' data for '{tournament_name}'")
+    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+    response = requests.get(url)
+    slug = tournament_name.replace(" ", "-").lower()
+    path = RAW_DATA_DIR.joinpath(f"{slug}.csv")
+
+    with open(path, "w") as f:
+        f.write(response.text)
+
+    return path
+
+
+if __name__ == "__main__":
+    get_player_stats(
+        "18eJUXRPuJQCVEsEukqDtB3PMy--CmzSZcIX9QJO4gfY",
+        "Pools",
+        "NCS 22-23 Mixed Regionals South",
+    )
