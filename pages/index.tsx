00aba1c24cd7b719f84f69d72cca97896cc49e93
Puneeth Chaganti <punchagan@muse-amuse.in>
2023-01-09 00:56:37 +0530

    index: Fix icon link to point to India Ultimate site

--- pages/index.tsx
+++ pages/index.tsx
@@ -92,7 +92,7 @@ export default function Home() {
                     <p>Scores for India Ultimate Tournaments</p>
                     <div>
                         <a
-                            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                            href="https://indiaultimate.org"
                             target="_blank"
                             rel="noopener noreferrer"
                         >
